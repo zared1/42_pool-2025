51c4068f2855aa6ce1df8642d48ad75765453d38
ZARED1 <168565323+zared1@users.noreply.github.com>
2025-08-30 14:16:38 +0100

    Add files via upload

---
+++ c05/ex04/ft_fibonacci.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zridoine <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/30 15:03:38 by zridoine          #+#    #+#             */
+/*   Updated: 2025/08/02 13:12:44 by zridoine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index == 0)
+		return (0);
+	if (index == 1)
+		return (1);
+	if (index < 0)
+		return (-1);
+	else
+		return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
