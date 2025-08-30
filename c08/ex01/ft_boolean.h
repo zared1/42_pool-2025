8231aedc2a15b64e68dcb14601f87f2de1bcf4e6
ZARED1 <168565323+zared1@users.noreply.github.com>
2025-08-30 14:33:50 +0100

    Add files via upload

---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zridoine <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/05 14:54:06 by zridoine          #+#    #+#             */
+/*   Updated: 2025/08/05 15:00:14 by zridoine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+# include <unistd.h>
+# define t_bool int
+# define EVEN(n) (n % 2 == 0)
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS 1
+# define EVEN_MSG "I have an even number of arguments.\n" 
+# define ODD_MSG "I have an odd number of arguments.\n"
+#endif
